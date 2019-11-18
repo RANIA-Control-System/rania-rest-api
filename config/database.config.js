2ceb12748e163520932881d05906ce174d09ae16
Victor Perez <vjperez@mix.wvu.edu>
2019-11-18 15:41:01 -0500

    Delete database.config.js

--- config/database.config.js
+++
@@ -1,5 +0,0 @@
-//following this guide: https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
-module.exports = {
-  url:
-    "mongodb+srv://application-user:A8SNX5e6lB8Aaq0N@481group1-gqzp2.mongodb.net/test?retryWrites=true&w=majority"
-};
