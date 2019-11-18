f9d3cfcbdd276390ebea73626ce8dacf7747d228
Victor Perez <vjperez@mix.wvu.edu>
2019-11-18 15:36:13 -0500

    Added create/update requsts

--- app/controllers/patient.controller.js
+++ app/controllers/patient.controller.js
@@ -1,9 +1,40 @@
 const Patient = require("../models/patient.model.js");
-//For all thesse functions, req stands for request and res stands for result
+//For all these functions, req stands for request and res stands for result
 
 // Create and Save a new patient
 exports.create = (req, res) => {
-  //@TODO
+  // Validate request
+  if (!req.body.content) {
+    return res.status(400).send({
+      message: "Patient content can not be empty"
+    });
+  }
+
+  // Create a Patient
+  const patient = new Patient({
+    name: {
+      firstName: req.body.firstName,
+      middleInitial: req.body.middleInitial,
+      lastName: req.body.lastName
+    },
+    birthDate: req.body.birthDate,
+    phoneNumber: req.body.phoneNumber,
+    gender: req.body.gender,
+    maritalStatus: req.body.maritalStatus
+  });
+
+  // Save Patient in the database
+  patient
+    .save()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Patient."
+      });
+    });
 };
 
 // Retrieve and return all patients from the database.
@@ -47,7 +78,47 @@ exports.findOne = (req, res) => {
 
 // Update a patient patient by the patientId in the request
 exports.update = (req, res) => {
-  //@TODO
+  // Validate Request
+  if (!req.body.content) {
+    return res.status(400).send({
+      message: "Patient content can not be empty"
+    });
+  }
+
+  // Find patient and update it with the request body
+  Patient.findByIdAndUpdate(
+    req.params.patientId,
+    {
+      name: {
+        firstName: req.body.firstName,
+        middleInitial: req.body.middleInitial,
+        lastName: req.body.lastName
+      },
+      birthDate: req.body.birthDate,
+      phoneNumber: req.body.phoneNumber,
+      gender: req.body.gender,
+      maritalStatus: req.body.maritalStatus
+    },
+    { new: true }
+  )
+    .then(patient => {
+      if (!patient) {
+        return res.status(404).send({
+          message: "Patient not found with id " + req.params.patientId
+        });
+      }
+      res.send(patient);
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "patient not found with id " + req.params.patientId
+        });
+      }
+      return res.status(500).send({
+        message: "Error updating patient with id " + req.params.patientId
+      });
+    });
 };
 
 // Delete a patient with the specified patientId in the request
